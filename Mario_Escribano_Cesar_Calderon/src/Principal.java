8824d6aa53351830f9111c410df143ddce5c9a9d
Mariokiller <mescribanor2320@gmail.com>
2022-03-08 14:09:08 +0100

    Commit tercero prueba

--- Mario_Escribano_Cesar_Calderon/src/Principal.java
+++ Mario_Escribano_Cesar_Calderon/src/Principal.java
@@ -2,6 +2,6 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola me llamo Mario Escribano Rejas");
+		System.out.println("Hola me llamo Mario Escribano Rejas, project leader");
 	}
 }
